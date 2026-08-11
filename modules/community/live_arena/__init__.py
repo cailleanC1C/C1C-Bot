69ae8c5993075a1cfe73b5a8247542fa808c165a
Caillean <227417825+cailleanC1C@users.noreply.github.com>
2026-08-11 17:05:37 +0200

    feat(live-arena): install Swiss organizer controls

--- modules/community/live_arena/__init__.py
+++ modules/community/live_arena/__init__.py
@@ -3,7 +3,9 @@
 from modules.community.live_arena.runtime_hooks import install as _install_runtime_hooks
 from modules.community.live_arena.competition_repair import install as _install_repair
 from modules.community.live_arena.competition_followup import install as _install_followup
+from modules.community.live_arena.swiss_panel import install as _install_swiss
 
 _install_runtime_hooks()
 _install_repair()
 _install_followup()
+_install_swiss()
