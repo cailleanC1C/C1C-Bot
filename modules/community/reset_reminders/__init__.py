0645f568e0cc1164625bde35ad2e8eb0f9dc801d
Caillean <227417825+cailleanC1C@users.noreply.github.com>
2026-08-23 13:24:06 +0200

    wire reset reminder panel rollover lifecycle

--- modules/community/reset_reminders/__init__.py
+++ modules/community/reset_reminders/__init__.py
@@ -1,10 +1,13 @@
 from .models import ResetReminder
-from .scheduler import (
-    process_reset_reminders,
-    register_persistent_reset_views,
-    schedule_reset_reminder_jobs,
-)
 from .views import ResetReminderView
+from . import scheduler as _scheduler
+from .panel_rollover import install_reset_reminder_panel_rollover
+
+install_reset_reminder_panel_rollover(_scheduler)
+
+process_reset_reminders = _scheduler.process_reset_reminders
+register_persistent_reset_views = _scheduler.register_persistent_reset_views
+schedule_reset_reminder_jobs = _scheduler.schedule_reset_reminder_jobs
 
 __all__ = [
     "ResetReminder",
